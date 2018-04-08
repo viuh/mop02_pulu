548eae28f41cb8547712f420221f67384ac61f76
viuh <37941072+viuh@users.noreply.github.com>
2018-04-08 16:24:35 +0300

    :leaf:

---
+++ src/components/Person.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+const Person = ({name,phone}) => {
+    //console.log("PEr",name,";", phone)
+    return (
+        <tr><td>{name}</td><td>{phone}</td></tr>
+      )
+  
+  }
+  
+
+
+
+
+export default Person
---
+++ src/index.js
@@ -0,0 +1,126 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Person from './components/Person'
+import SearchForm from './components/SearchForm'
+import Header from './components/Header'
+
+
+class App extends React.Component {
+    constructor(props) {
+      super(props)
+      this.state = {
+          persons: 
+          [
+            { name: 'Arto Hellas',
+              phone: '123-35234' }
+          ]   ,
+          newName: '',
+          newPhone: '',
+          filter: '',
+        }
+      ;
+
+    }
+  
+    addPerson = (event) => {
+      event.preventDefault()
+      
+      let newperson = {name: this.state.newName,
+        phone: this.state.newPhone}
+      
+      let temp = this.state.persons
+    
+      let foundList = temp.filter(person=>person.name.startsWith(this.state.newName)===true)
+    
+      if (foundList.length===0) {
+        temp.push(newperson)
+        this.setState({persons: temp, newName:'', newPhone:''})
+      } else {
+        alert("Name exists already!")
+      }
+    
+    }
+    
+
+    handleFilter = (event) => {
+      this.setState({ filter: event.target.value})
+    }
+
+    handleName = (event) => {
+      let temp = event.target.value
+
+      let orig = this.newName || ''
+
+      let newVal = orig + temp 
+      this.setState({newName: newVal})
+      //
+    }
+    
+    handlePhone = (event) => {
+      let temp = event.target.value
+
+      let orig = this.newPhone || ''
+
+      let newVal = orig + temp 
+      this.setState({newPhone: newVal})
+      //
+    }
+
+    listAll = () => { 
+      //console.log("LA:",this.state.persons)
+      let list = this.state.persons
+
+      if (this.state.filter.length > 0) {
+        list = this.state.persons.filter(person=>person.name.startsWith(this.state.filter)===true)
+
+      } 
+      
+      return (
+        list.map(person =>
+        <Person key={person.name} name={person.name} phone={person.phone}/>
+        )
+      ) 
+    }
+
+    render() {
+
+      return (
+        <div>
+          <Header text="Puhelinluettelo"/>
+  
+          <SearchForm fu1={this.state.newFilter} 
+            fu2={this.handleFilter}/>
+
+          <h2>Lisää uusi</h2>
+          <form onSubmit={this.addPerson}>
+          <div>
+            nimi: <input value={this.state.newName} 
+            onChange={this.handleName}/> <br/>
+            numero: <input value={this.state.newPhone} 
+            onChange={this.handlePhone} />
+          </div>
+          <div>
+            <button type="submit" >lisää</button>
+          </div>
+          </form>
+
+          <Header text="Numerot"/>
+          <table><tbody>
+            {this.listAll()}
+          </tbody>
+          </table>
+
+        </div>
+      )
+    }
+  }
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
+
+
+
+export default App
